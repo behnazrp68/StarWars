883a67179bee108c3444beaa43c30cc13020b98f
behnazrp68 <behnaz.rajabi.p@gmail.com>
2025-01-19 19:42:03 +0330

    add spacer to search screen

--- app/src/main/java/com/rajabi/starwars/presentation/compose/SearchScreen.kt
+++ app/src/main/java/com/rajabi/starwars/presentation/compose/SearchScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +25,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.rajabi.starwars.data.model.Character
 import com.rajabi.starwars.presentation.viewmodel.search.SearchUiAction
 import com.rajabi.starwars.presentation.viewmodel.search.SearchUiEvent
 import com.rajabi.starwars.presentation.viewmodel.search.SearchUiState
@@ -36,23 +36,19 @@ import java.nio.charset.StandardCharsets
 @Composable
 fun SearchScreen(navController: NavHostController, viewModel: SearchViewModel = hiltViewModel()) {
     val uiState by viewModel.uiState.collectAsState()
-    val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
+
     LaunchedEffect(Unit) {
         viewModel.uiEvent.collect { event ->
-            when (event) {
-                is SearchUiEvent.ShowToast -> Toast.makeText(
-                    context,
-                    event.message,
-                    Toast.LENGTH_SHORT
-                ).show()
+            if (event is SearchUiEvent.ShowToast) {
+                Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+    var query by remember { mutableStateOf("") }
+    Spacer(modifier = Modifier.height(20.dp))
     Column(modifier = Modifier.padding(16.dp)) {
-        var query by remember { mutableStateOf("") }
-
         // Search Input
         TextField(
             value = query,
@@ -72,46 +68,51 @@ fun SearchScreen(navController: NavHostController, viewModel: SearchViewModel = 
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Handle UI State
+        // UI State Handling
         when (val state = uiState) {
             is SearchUiState.Idle -> Text("Enter a query to search characters.")
             is SearchUiState.Loading -> CircularProgressIndicator(
-                modifier = Modifier.align(
-                    Alignment.CenterHorizontally
-                )
+                modifier = Modifier.align(Alignment.CenterHorizontally)
             )
 
-            is SearchUiState.Success -> {
-                if (state.characters.isEmpty()) {
-                    Text(
-                        "No characters found.",
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-                } else {
-                    LazyColumn {
-                        items(state.characters) { character ->
-                            val index = state.characters.indexOf(character)
-                            Column(modifier = Modifier
-                                .padding(8.dp)
-                                .clickable {
-                                    val id = character.url.trimEnd('/').substringAfterLast('/')
-                                    val encodedUrl = URLEncoder.encode(character.url, StandardCharsets.UTF_8.toString())
-                                    navController.navigate("character_detail/${encodedUrl}")
-                                }) {
-                                Text("Name: ${character.name}")
-                                Text("Birth Year: ${character.birthYear}")
-                                Spacer(modifier = Modifier.height(8.dp))
-                            }
-                        }
-                    }
-                }
-            }
-
+            is SearchUiState.Success -> CharacterList(state.characters, navController)
             is SearchUiState.Error -> Text(
                 text = "Error: ${state.message}",
                 color = MaterialTheme.colorScheme.error,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
         }
+    }
+}
+
+@Composable
+fun CharacterList(characters: List<Character>, navController: NavHostController) {
+    if (characters.isEmpty()) {
+        Text("No characters found.")
+    } else {
+        LazyColumn {
+            items(characters) { character ->
+                CharacterItem(character, navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun CharacterItem(
+    character: com.rajabi.starwars.data.model.Character,
+    navController: NavHostController
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable {
+                val encodedUrl = URLEncoder.encode(character.url, StandardCharsets.UTF_8.toString())
+                navController.navigate("character_detail/$encodedUrl")
+            }
+    ) {
+        Text("Name: ${character.name}")
+        Text("Birth Year: ${character.birthYear}")
+        Spacer(modifier = Modifier.height(8.dp))
     }
 }
